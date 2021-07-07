49ae2bf6fc5653a4139a216f50e9b74594739d59
drewml08 <drewml08@gmail.com>
2021-07-07 00:10:08 -0400

    added prompts for each role, updated employee and manager to use promise objects, added module exports for js files

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
 var fs = require('fs');
-
 const inquirer = require('inquirer');
+const Manager = require("./lib/Manager");
+
+var team = [];
+
+var manager = new Manager ();
+
+manager.prompt().then( () => {
+    team.push(manager);
+    console.log(manager);
+});
