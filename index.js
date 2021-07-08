9d06e78f98e5e7939fad5a8bb22c369ed1569a6a
drewml08 <drewml08@gmail.com>
2021-07-07 23:39:36 -0400

    added logic and functionality for adding additional team members after manager

--- index.js
+++ index.js
@@ -1,12 +1,56 @@
 var fs = require('fs');
+
 const inquirer = require('inquirer');
+
 const Manager = require("./lib/Manager");
+
+const Engineer = require("./lib/Engineer");
+
+const Intern = require("./lib/Intern");
+
+const addQuestion = [
+    {
+        type: 'list',
+        message: 'What type of team member would you like to add?',
+        choices: ['Engineer', 'Intern','Finish building team'],
+        name: 'newMember',
+    },
+];
 
 var team = [];
 
 var manager = new Manager ();
 
-manager.prompt().then( () => {
+function askTeamMember() {
+    return new Promise ( (resolve, reject) => {
+        inquirer.prompt(addQuestion).then((response) => {
+        resolve(response.newMember);
+        });
+    });
+}
+
+manager.prompt().then( async () => {
     team.push(manager);
-    console.log(manager);
+    var done = false;
+    while (!done){
+        var answer = await askTeamMember(); 
+        var teamMember = null;
+        switch (answer) {
+            case "Engineer":
+                teamMember = new Engineer();
+                break;
+
+            case "Intern":
+                teamMember = new Intern();
+                break;
+
+            case "Finish building team":
+                done = true;
+                break;            
+        }
+        if (teamMember) {
+            await teamMember.prompt();
+            team.push(teamMember);
+        }  
+    }
 });
